7f6ac1aa338eda7063cdbd1c6b6354b47507d93f
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-05-20 11:43:52 +0300

    As a director i can make subjects, so that grades have subjects [Delivers #181803864]

--- classes.py
+++ classes.py
@@ -1,11 +1,21 @@
 class School:
     name = ""
     grade = []
+    subject = []
     def adding_name(self):
         self.name = input("Name the school:")
     def adding_grades(self,grade):
-        self.grade.append(grade)
+        self.grade.append(grade.name)
+    def adding_subject(self,subject):
+        self.subject.append(subject.name)
 class Grade:
     name = ""
     def naming_grade(self):
         self.name = input(str("Enter grade name:"))
+class Subject:
+    name = ""
+    required_to_study = []
+    def naming_subject(self):
+        self.name = input(str("Enter subject name:"))
+    def add_grade_to_subject(self,grade):
+        self.required_to_study.append(grade)
