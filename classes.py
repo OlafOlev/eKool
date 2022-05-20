885cc6e89e8db37edd7848110dacbdd7dfd83252
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-05-20 12:45:53 +0300

    As a director I can hire teachers, so that school has teachers [Delivers #181803633]

--- classes.py
+++ classes.py
@@ -2,12 +2,15 @@ class School:
     name = ""
     grade = []
     subject = []
+    teacher = []
     def adding_name(self):
         self.name = input("Name the school:")
     def adding_grades(self,grade):
         self.grade.append(grade.name)
     def adding_subject(self,subject):
         self.subject.append(subject.name)
+    def adding_teacher(self,teacher):
+        self.teacher.append(teacher.name)
 class Grade:
     name = ""
     def naming_grade(self):
@@ -15,7 +18,20 @@ class Grade:
 class Subject:
     name = ""
     required_to_study = []
+    teacher = []
     def naming_subject(self):
         self.name = input(str("Enter subject name:"))
     def add_grade_to_subject(self,grade):
-        self.required_to_study.append(grade)
+        self.required_to_study.append(grade.name)
+    def adding_teacher(self, teacher):
+        self.teacher.append(teacher.name)
+class Teacher:
+    name = ""
+    schools = []
+    subjects = []
+    def naming_teachers(self):
+        self.name = input(str("Enter teacher name:"))
+    def teaching_subjects(self, subject):
+        self.subjects.append(subject.name)
+    def teaching_school(self, school):
+        self.schools.append(school.name)
