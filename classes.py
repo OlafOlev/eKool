0c4ca38eb02584dd3235a5c471d335614c8ef87e
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-05-20 11:28:19 +0300

    As a director, I can add different grades(like ITA21), so that the school has different grades [Delivers #181803569]

--- classes.py
+++ classes.py
@@ -1,3 +1,11 @@
-from main import *
-school = School()
-school.adding_name(input("Name the school:"))
+class School:
+    name = ""
+    grade = []
+    def adding_name(self):
+        self.name = input("Name the school:")
+    def adding_grades(self,grade):
+        self.grade.append(grade)
+class Grade:
+    name = ""
+    def naming_grade(self):
+        self.name = input(str("Enter grade name:"))
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-class School:
-    name = ""
-    def adding_name(self, name):
-        self.name = name
+from classes import *
+school = School()
+grade = Grade()
+school.adding_name()
+grade.naming_grade()
+school.adding_grades(grade)
+print(school.name)
+print(grade.name)
