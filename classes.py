77fcbd77cf7115ef0e3a9c3d568462c547685cfb
Olaf Olev Alakivi <olaf-olev.alakivi@khk.ee>
2022-05-31 03:34:15 +0300

    As a director, I can see better outputs so that its easier to understand [Delivers #182326256]

--- classes.py
+++ classes.py
@@ -1,27 +1,40 @@
 class School:
-    name = ""
-    grade = []
-    subject = []
-    teacher = []
-    students = []
+    def __init__(self):
+        self.name = ""
+        self.grade = []
+        self.subject = []
+        self.teacher = []
+        self.students = []
+        self.ranked_students = []
     def adding_name(self):
         self.name = input("Name the school:")
-    def adding_grades(self,grade):
-        self.grade.append(grade.name)
-    def adding_subject(self,subject):
-        self.subject.append(subject.name)
-    def adding_teacher(self,teacher):
-        self.teacher.append(teacher.name)
+    def adding_grades(self, grade):
+        self.grade.append(grade)
+    def adding_subject(self, subject):
+        self.subject.append(subject)
+    def adding_teacher(self, teacher):
+        self.teacher.append(teacher)
+        teacher.teaching_school(self)
     def registering_student_2school(self, student):
-        self.students.append(student.student_name)
+        if student.student_school == "":
+            self.students.append(student)
+            student.student_school = self
     def show_grades(self):
-        return self.grade
+        print("---" + f"This is grade school list")
+        for i in self.grade:
+            print(i.name)
     def show_students(self):
-        return self.students
+        print("---" + f"This is student school list")
+        for i in self.students:
+            print(i.student_name)
     def show_teacher(self):
-        return self.teacher
+        print("---" + f"This is teacher school list")
+        for i in self.teacher:
+            print(i.name)
     def show_subject(self):
-        return self.subject
+        print("---" + f"This is subject school list")
+        for i in self.subject:
+            print(i.name)
     def getmark(self, student):
         return student.return_avg_mark()
     def getranked(self, students, school):
@@ -36,64 +49,103 @@ class School:
             print(str(g) + ".", i.student_name,f"average grade is ", str(i.student_avg_mark))
             g += 1
 class Grade:
-    name = ""
-    students = []
-    def naming_grade(self):
-        self.name = input(str("Enter grade name:"))
-    def students_learning_thegrade(self, student):
+    def __init__(self):
+        self.name = ""
+        self.students = []
+        self.grade_required_to_study = []
+    def naming_grade(self, name):
+        self.name = name
+    def students_learning_the_grade(self, student):
         self.students.append(student)
-        student.adding_grade(self.name)
+    def add_subject_to_grade(self, subject):
+        self.grade_required_to_study.append(subject)
 class Subject:
-    name = ""
-    required_to_study = []
-    teacher = []
-    students = []
-    def naming_subject(self):
-        self.name = input(str("Enter subject name:"))
-    def add_grade_to_subject(self,grade):
-        self.required_to_study.append(grade.name)
+    def __init__(self):
+        self.name = ""
+        self.required_to_study = []
+        self.teacher = []
+        self.students = []
+    def naming_subject(self, name):
+        self.name = name
+    def add_grade_to_subject(self, grade):
+        self.required_to_study.append(grade)
+        grade.add_subject_to_grade(self)
     def adding_teacher(self, teacher):
-        self.teacher.append(teacher.name)
-    def registering_student_2subject(self,student):
-        for i in self.required_to_study:
+        self.teacher.append(teacher)
+        teacher.teaching_subjects(self)
+    def registering_student_2subject(self, student, subject):
+        for i in subject.required_to_study:
             if i == student.student_grade:
-                self.students.append(student.student_name)
-                student.adding_subject(self.name)
+                subject.students.append(student)
+                student.adding_subject(student)
             else:
-                print("You cant study this")
+                print("---" + "You cant study this")
 class Teacher:
-    name = ""
-    schools = []
-    subjects = []
+    def __init__(self):
+        self.name = ""
+        self.schools = []
+        self.subjects = []
     def naming_teachers(self):
         self.name = input(str("Enter teacher name:"))
     def teaching_subjects(self, subject):
-        self.subjects.append(subject.name)
+        self.subjects.append(subject)
     def teaching_school(self, school):
-        self.schools.append(school.name)
+        self.schools.append(school)
+    def show_subjects(self):
+        print("---" + self.name, f"teaches these subjects")
+        for i in self.subjects:
+            print(i.name)
 class Student:
-    student_name = ""
-    student_grade = ""
-    student_marks = [] #grade as "You got 2 on your test"
-    student_avg_mark = 0
-    student_mark_total = 0
-    def getting_avg_mark(self):
-        for i in self.student_marks:
+    def __init__(self):
+        self.student_school = ""
+        self.student_avg_mark = 0
+        self.student_name = ""
+        self.student_grade = ""
+        self.student_marks = [] #grade as "You got 2 on your test"
+        self.student_mark_total = 0
+        self.student_subjects = []
+        self.failed_subjects = []
+        self.passed_subjects = []
+        self.student_required_to_study = []
+    def return_avg_mark(self):
+        return self.student_avg_mark
+    def getting_avg_mark(self, student):
+        for i in student.student_marks:
             self.student_mark_total += i
         self.student_avg_mark = round(self.student_mark_total/len(self.student_marks),2)
-        return self.student_avg_mark
-    student_subjects = []
-    failed_subjects = []
-    passed_subjects = []
+        return student.student_avg_mark
     def naming_student(self):
         self.student_name = input(str("Enter student name:"))
-    def adding_subject(self, subject):
-        self.student_subjects.append(subject)
+    def  registering_to_subject(self, subject):
+        for i in self.student_required_to_study:
+            if i == subject:
+                self.student_subjects.append(subject)
     def adding_grade(self, grade):
         self.student_grade = grade
+        self.student_required_to_study = grade.grade_required_to_study
+        grade.students_learning_the_grade(self)
     def passed_the_subject(self, subject):
-        self.passed_subjects.append(subject.name)
+        self.passed_subjects.append(subject)
     def failed_the_subject(self, subject):
-        self.failed_subjects.append(subject.name)
-    def add_a_mark(self, mark):
-        self.student_marks.append(mark)
+        self.failed_subjects.append(subject)
+    def add_a_mark(self, mark, subject):
+        if mark == 1 or mark == 2 or mark == 3 or mark == 4 or mark == 5:
+            self.student_marks.append(mark)
+            if mark == 1 or mark == 2:
+                self.failed_the_subject(subject)
+            if mark == 3 or mark == 4 or mark == 5:
+                self.passed_the_subject(subject)
+    def show_subject_stats(self):
+        print("---" + self.student_name, f"subject status is:")
+        if self.failed_subjects.__len__() == 0:
+            print(f"---No failed subjects")
+        else:
+            print(f"---Failed subjects:")
+            for i in self.failed_subjects:
+                print(i.name)
+        if self.passed_subjects.__len__() == 0:
+            print(f"---No passed subjects")
+        else:
+            print(f"---Passed subjects:")
+            for i in self.passed_subjects:
+                print(i.name)
--- main.py
+++ main.py
@@ -1,52 +1,121 @@
 from classes import *
+#Making first school and naming it
 school = School()
-grade = Grade()
-subject_proge = Subject()
-teacher = Teacher()
 school.adding_name()
-grade.naming_grade()
-teacher.naming_teachers()
-student = Student()
-student.naming_student()
-school.adding_grades(grade)
-school.adding_teacher(teacher)
-subject_proge.naming_subject()
-subject_proge.add_grade_to_subject(grade)
-school.adding_subject(subject_proge)
-subject_proge.adding_teacher(teacher)
-teacher.teaching_subjects(subject_proge)
-teacher.teaching_school(school)
-school.registering_student_2school(student)
-grade.students_learning_thegrade(student)
-subject_proge.registering_student_2subject(student)
-print(school.name)
-print(grade.name)
-print(school.grade)
-print(school.subject)
-print(school.teacher)
-print(school.students)
-print(subject_proge.name)
-print(subject_proge.teacher)
-print(subject_proge.required_to_study)
-print(subject_proge.students)
-print(teacher.name)
-print(teacher.schools)
-print(teacher.subjects)
-print(student.student_name)
-print(student.student_grade)
-print(student.student_subjects)
-student.passed_the_subject(subject_proge)
-student.failed_the_subject(subject_proge)
-print(student.passed_subjects)
-print(student.failed_subjects)
-student.add_a_mark(1)
-student.add_a_mark(2)
-student.add_a_mark(3)
-student.add_a_mark(4)
-student.add_a_mark(5)
-print(student.student_marks)
-print(student.getting_avg_mark())
-print(school.show_grades())
-print(school.show_subject())
-print(school.show_students())
-print(school.show_teacher())
+#Lists
+all_teachers_list = []
+all_students_list = []
+#Creating bunch of teachers and adding them to school
+for i in range(5):
+    teacher = Teacher()
+    teacher.naming_teachers()
+    school.adding_teacher(teacher)
+    all_teachers_list.append(teacher)
+#Creating bunch of student
+for i in range(10):
+    student = Student()
+    student.naming_student()
+    all_students_list.append(student)
+#Creating second school
+school2 = School()
+school2.adding_name()
+#Adding all the students to the first school
+for i in all_students_list:
+    school.registering_student_2school(i)
+#Trying to add all the students into the second school
+for i in all_students_list:
+    school2.registering_student_2school(i)
+#Checking if adding all the students into the second school failed or not
+if school2.students.__len__() == 0:
+    print(f"The second school could not register students to the second school")
+#Creating different subjects and adding them to school and adding teachers who teach the subject
+#First Subject
+pe = Subject()
+pe.naming_subject("pe")
+school.adding_subject(pe)
+pe.adding_teacher(school.teacher[0])
+pe.adding_teacher(school.teacher[1])
+pe.adding_teacher(school.teacher[2])
+#Second Subject
+it = Subject()
+it.naming_subject("it")
+school.adding_subject(it)
+it.adding_teacher(school.teacher[3])
+#Third Subject
+cooking_class = Subject()
+cooking_class.naming_subject("cooking class")
+school.adding_subject(cooking_class)
+cooking_class.adding_teacher(school.teacher[4])
+#Creating different grades where people could study
+itdeveloper = Grade()
+itdeveloper.naming_grade("IT Developer")
+school.adding_grades(itdeveloper)
+chef = Grade()
+chef.naming_grade("Chef")
+school.adding_grades(chef)
+#Adding subject that the x grade has to learn
+pe.add_grade_to_subject(itdeveloper)
+pe.add_grade_to_subject(chef)
+it.add_grade_to_subject(itdeveloper)
+cooking_class.add_grade_to_subject(chef)
+#Adding students to their grades
+b = 0
+for i in school.students:
+    if b < 5:
+        i.adding_grade(itdeveloper)
+    if b >= 5:
+        i.adding_grade(chef)
+    b = b + 1
+#Students registering to subjects
+for i in itdeveloper.students:
+    i.registering_to_subject(it)
+    i.registering_to_subject(pe)
+for i in chef.students:
+    i.registering_to_subject(cooking_class)
+    i.registering_to_subject(pe)
+#Students getting marks/grades for subjects
+c = 0
+for i in itdeveloper.students:
+    if c == 0:
+        i.add_a_mark(1, it)
+        i.add_a_mark(2, pe)
+    if c == 1:
+        i.add_a_mark(3, it)
+        i.add_a_mark(4, pe)
+    if c == 5:
+        i.add_a_mark(5, it)
+        i.add_a_mark(5, pe)
+    elif c > 1:
+        i.add_a_mark(3, it)
+        i.add_a_mark(3, pe)
+    c = c + 1
+d = 0
+for i in chef.students:
+    if d == 0:
+        i.add_a_mark(1, cooking_class)
+        i.add_a_mark(2,pe)
+    if d == 1:
+        i.add_a_mark(3, cooking_class)
+        i.add_a_mark(4,pe)
+    if d == 5:
+        i.add_a_mark(5, cooking_class)
+        i.add_a_mark(5, pe)
+    elif d > 1:
+        i.add_a_mark(3, cooking_class)
+        i.add_a_mark(3, pe)
+    d = d + 1
+#Checking the students subject status
+for i in itdeveloper.students:
+    i.show_subject_stats()
+for i in chef.students:
+    i.show_subject_stats()
+#Showing teacher, student and subject lists
+school.show_grades()
+school.show_teacher()
+school.show_subject()
+school.show_students()
+#Showing what teacher teaches what
+for i in school.teacher:
+    i.show_subjects()
+#Show school ranking
+school.show_school_ranking(school, school.students)
